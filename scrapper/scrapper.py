71709792d6886073fd820ba1998ea4119b912396
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2018-01-02 17:38:02 +0000

    added chart js to the application

--- scrapper/scrapper.py
+++ scrapper/scrapper.py
@@ -1,26 +1,7 @@
-# import twitter
-# import os
-#
-# consumer_key = os.environ.get('SENTI_CONSUMER_KEY')
-# consumer_secret = os.environ.get('SENTI_CONSUMER_SECRET')
-# access_token = os.environ.get('SENTI_ACCESS_TOKEN')
-# access_token_secret = os.environ.get('SENTI_ACCESS_TOKEN_SECRET')
-#
-# api = twitter.Api(consumer_key=[consumer_key],
-#                   consumer_secret=[consumer_secret],
-#                   access_token_key=[access_token],
-#                   access_token_secret=[access_token_secret])
-#
-# def get_tweets():
-#     """
-#     returns twitter feed with settings as described below, contains all related twitter settings
-#     """
-#     # print(sys.stderr, 'hi')
-#     # print(sys.stderr, api.GetUserTimeline(screen_name='AdrianPonczek', exclude_replies=True, include_rts=False))
-#     return api.GetUserTimeline(screen_name='@AdrianPonczek', exclude_replies=True, include_rts=False)  # includes entities
-
 import tweepy
 import os
+import json
+
 consumer_key = os.environ.get('SENTI_CONSUMER_KEY')
 consumer_secret = os.environ.get('SENTI_CONSUMER_SECRET')
 access_token = os.environ.get('SENTI_ACCESS_TOKEN')
@@ -34,15 +15,30 @@ api = tweepy.API(auth)
 
 
 # public_tweets = api.home_timeline()
-public_tweets = api.search(q='trump')
+# public_tweets = api.search(q='trump')
+
+# streamer = Streamer(auth, callback)
 
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status.text)
+        print(status)
+        return True
 
+    def on_data(self, tweet):
+        print(json.loads(tweet))
+    # def on_data(self, status):
+    #     self.output[status.id] = {
+    #
+    #     }
+    #
+    #     return True
 
 def get_tweets(query):
+    myStreamListener = MyStreamListener()
+    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
+    myStream.filter(track=[query], async=True)
+    # print(myStream.filter(track=[query], async=True))
     tweets = api.search(q=query)
 
     # myStreamListener = MyStreamListener()
