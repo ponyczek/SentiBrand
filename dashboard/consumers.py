c3d616cf343b2b0713f857c0bf35e4e7e946b965
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2018-01-07 21:28:58 +0000

    progress done on multithreaded search with websockets

--- dashboard/consumers.py
+++ dashboard/consumers.py
@@ -1,14 +1,10 @@
 from channels import Group
 from channels.auth import channel_session_user, channel_session_user_from_http, channel_session
 import json
-import scrapper
 import tweepy
 import os
-import time
-from django.conf import settings
-from django.contrib.sessions.backends.db import SessionStore
 import threading
-import time
+from channels.generic.websockets import WebsocketDemultiplexer
 
 
 consumer_key = os.environ.get('SENTI_CONSUMER_KEY')
@@ -21,24 +17,6 @@ auth.set_access_token(access_token, access_token_secret)
 
 
 api = tweepy.API(auth)
-
-class MyStreamListener(tweepy.StreamListener):
-
-    def on_status(self, status):
-        print(status)
-        return True
-
-    def on_data(self, tweet):
-        # data = json.loads(HTMLParser().unescape(data))
-        # s = SessionStore()
-        print("bla" + str(type(tweet)))
-        # Group('dashboard').send({'text': tweet})
-        # print(s)
-        # data = json.loads(tweet)
-        # return json.loads(tweet)
-
-        # return True
-
 
 class perpetualTimer():
 
@@ -59,44 +37,46 @@ class perpetualTimer():
    def cancel(self):
       self.thread.cancel()
 
+class WsThread(WebsocketDemultiplexer):
+    http_user = True
 
-def hello_world(query_phrase):
+    def connection_groups(self, **kwargs):
+        pass
+
+
+    def receive(self, message, **kwargs):
+        try:
+
+            t = perpetualTimer(2, lambda: hello_world(message))
+            t.start()
+        except ValueError:
+            print("ws message isn't json text=%s", message['text'])
+            return
+
+
+def hello_world(message):
+
+    query_phrase = message['text']
+    group_name = message['user_name'] + "_search"
+
     tweets = api.search(q=query_phrase)
     status = tweets[0]
-    Group('dashboard').send({"text": json.dumps(status._json)})
+    Group(group_name).send({"text": json.dumps(status._json)})
     print(status)
-    # return status
 
 t = perpetualTimer(2, lambda: hello_world("") )
 
-# t = perpetualTimer(2, hello_world)
-# print("hi")
-# t.start()
 
 @channel_session_user_from_http
 def ws_connect(message):
-    print("connected")
-
+    group_name = message.user.username + "_search"
     message.reply_channel.send({
         "text": json.dumps({
             "action": "reply_channel",
             "reply_channel": message.reply_channel.name,
         })
     })
-    Group('dashboard').add(message.reply_channel)
-
-
-@channel_session
-def ws_receive(message):
-    try:
-        print('kek')
-        data = json.loads(message['text'])
-        t = perpetualTimer(2, lambda: hello_world(data['text']))
-        t.start()
-        # print(data['text'])
-    except ValueError:
-       print("ws message isn't json text=%s", message['text'])
-       return
+    Group(group_name).add(message.reply_channel)
 
 
 
@@ -108,7 +88,8 @@ def ws_disconnect(message):
             'is_logged_in': False
         })
     })
-    Group('dashboard').discard(message.reply_channel)
-    # myStream.disconnect()
+    group_name = message.user.username + "_search"
+
+    Group(group_name).discard(message.reply_channel)
     t.cancel()
     print('disconnected')
--- dashboard/routing.py
+++ dashboard/routing.py
@@ -1,10 +1,11 @@
-from channels.routing import route
-from dashboard.consumers import ws_connect, ws_disconnect, ws_receive
+from channels.routing import route, route_class
+from dashboard.consumers import ws_connect, ws_disconnect, WsThread
 
 
 channel_routing = [
     route('websocket.connect', ws_connect),
-    route('websocket.receive', ws_receive),
+    route_class(WsThread),
+    # route('websocket.receive', ws_receive),
     route('websocket.disconnect', ws_disconnect),
 
 ]
