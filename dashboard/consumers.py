695fa2c0c5858a0ec13562d085cf175ddb6359db
Adrian Grzywaczewski <1204969@rgu.ac.uk>
2018-02-04 23:45:56 +0000

    working on map and presentation of polarity

--- dashboard/consumers.py
+++ dashboard/consumers.py
@@ -1,15 +1,5 @@
-import json
-import os
-import threading
-
-import tweepy
-from channels import Group
 from channels.auth import channel_session_user, channel_session_user_from_http
-from channels.generic.websockets import WebsocketDemultiplexer
-# from .tasks import hello_world
-from SentiBrand.celery import app
 
-# from __future__ import absolute_import
 import json
 import tweepy
 import os
@@ -26,8 +16,6 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
-
-
 
 @channel_session_user_from_http
 def ws_connect(message):
@@ -98,6 +86,7 @@ def serialise_data(tweets):
             'id' : tweet.get('id'),
             'place': tweet.get('place'), #city country coordinates
 
+
         })
         print(tweet.get('place'))
 
@@ -109,7 +98,7 @@ def get_search_data(message, reply_channel):
     if message is not None and reply_channel is not None:
         query_phrase = message['text']
         last_id = message.get('last_tweet_id')
-        temp_tweets = api.search(q=query_phrase, since_id=last_id)
+        temp_tweets = api.search(q=query_phrase, since_id=last_id, lang='en')
         tweets = [tweet._json for tweet in temp_tweets ]
         data = serialise_data(tweets)
         Channel(reply_channel).send({"text": json.dumps(data)})
